d5ca9c4338edacd9003588366c723b307a19d0ec
Arthur Ribeiro Dias Fudali <arthurfudali5@gmail.com>
2024-11-10 21:51:16 -0300

    caminho da biblioteca connector

--- TPI/exerciciosConexaoBDeForm/projetoCRUD/src/view/FrmLogin.java
+++ TPI/exerciciosConexaoBDeForm/projetoCRUD/src/view/FrmLogin.java
@@ -7,6 +7,7 @@ package view;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import model.Usuario;
